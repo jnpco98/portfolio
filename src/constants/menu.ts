ac37496260199a185aa9ceb4b6b4f11614efae17
ukiyo-roa <jnpco.98@gmail.com>
2020-06-21 19:47:41 +1000

    Updated font and menu. Added button and gutters and added colors.

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -9,6 +9,7 @@ export type MenuItem = {
 
 export const mobilePrimaryMenu: MenuItem[] = [];
 export const mobileSecondaryMenu: MenuItem[] = [
+  { key: 'ms_home', link: '/', label: 'Home' },
   { key: 'ms_about', link: '#', label: 'About' },
   { key: 'ms_projects', link: '#', label: 'Projects' },
   { key: 'ms_contact', link: '#', label: 'Contact' },
@@ -16,10 +17,11 @@ export const mobileSecondaryMenu: MenuItem[] = [
 ];
 export const primaryMenu: MenuItem[] = [];
 export const secondaryMenu: MenuItem[] = [
-  { key: 'ms_about', link: '#', label: 'About' },
-  { key: 'ms_projects', link: '#', label: 'Projects' },
-  { key: 'ms_contact', link: '#', label: 'Contact' },
-  { key: 'ms_resume', link: '#', label: 'Resume' }
+  { key: 's_home', link: '/', label: 'Home' },
+  { key: 's_about', link: '#', label: 'About' },
+  { key: 's_projects', link: '#', label: 'Projects' },
+  { key: 's_contact', link: '#', label: 'Contact' },
+  { key: 's_resume', link: '#', label: 'Resume' }
 ];
 export const footerMenu: MenuItem[] = [
   { key: '', link: '', label: '' },
