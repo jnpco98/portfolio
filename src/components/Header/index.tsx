17fddf6db27d54fea18ccac0d4d80a213c6708a4
ukiyo-roa <jnpco.98@gmail.com>
2020-06-21 14:25:11 +1000

    Added style resets and def vars

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+function Header() {
+  return (
+    <div className="App">
+      <h1>header</h1>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.scss';
 
+import Header from '../../components/Header';
+
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    <React.Fragment>
+      <Header/>
+    </React.Fragment>
   );
 }
 
