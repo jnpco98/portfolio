d7b44a45d4be58b67bba21d40f581c2eb453ce78
ukiyo-roa <jnpco.98@gmail.com>
2020-06-22 20:24:12 +1000

    Integrated mail service api

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,35 @@
 import React, { useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
+import { MAIL_SERVICE } from '../constants/api';
 
 function Contact() {
   const [firstname, setFirstname] = useState('');
   const [lastname, setLastname] = useState('');
+  const [company, setCompany] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [mailResponse, setMailResponse] = useState('');
+  const [errorMailResponse, setErrorMailResponse] = useState('');
 
-  function handleSubmit(e: React.FormEvent) {
+  async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    console.log(firstname, lastname, email, message);
+
+    setMailResponse('');
+    setErrorMailResponse('');
+
+    const mailData = {
+      messageConfig: { firstname, lastname, email, message },
+      sendConfig: { companyName: company, agent: 'noreply.jnpco.98@gmail.com', receiver: 'jnpco.98@gmail.com' }
+    };
+
+    try {
+      const data = await fetch(MAIL_SERVICE, { method: 'POST', body: JSON.stringify(mailData) });
+      const res = await data.json();
+      if(res.error) throw new Error('');
+      setMailResponse('Message sent.');
+    } catch(e) {
+      setErrorMailResponse('Something went wrong. Try again later');
+    }
   }
 
   return(
@@ -24,8 +44,11 @@ function Contact() {
           <h2 className="c-contact__title">Contact me</h2>
           <input value={firstname} onChange={e => setFirstname(e.target.value)} placeholder="First name" />
           <input value={lastname} onChange={e => setLastname(e.target.value)} placeholder="Last name" />
+          <input value={company} onChange={e => setCompany(e.target.value)} placeholder="Company" />
           <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
           <TextareaAutosize value={message} onChange={e => setMessage(e.target.value)} placeholder="Message" minRows={5} />
+          {mailResponse && mailResponse.length && <p className="c-contact__form-success">{mailResponse}</p>}
+          {errorMailResponse && errorMailResponse.length && <p className="c-contact__form-error">{errorMailResponse}</p>}
           <button className="c-contact__form-cta o-btn" type="submit">Submit</button>
         </form>
     </section>
---
+++ src/constants/api.ts
@@ -0,0 +1,1 @@
+export const MAIL_SERVICE = 'https://39gr0fkrq7.execute-api.us-east-1.amazonaws.com/production/send-mail';
